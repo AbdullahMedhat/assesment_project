eaa6916ecb722583d8ae51b08fdd49dd1ee15d11
AbdullahMedhat <abdullahmedhat@outlook.com>
2017-05-21 16:25:47 +0200

    add admin auth model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Admin.create!(email: "a@a.com", password: "12345678", password_confirmation: "12345678", name: "Admin 1")
+Program.create!(name: "FireUp", description: "3 Months boot camp for web development")
+Program.create!(name: "GearUp", description: "Front end course")
