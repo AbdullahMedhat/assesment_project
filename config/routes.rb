f24e6a0787a413b78be4cbad382479a7022fcf9c
YOUR NAME <YOUR@EMAIL.com>
2017-05-25 18:19:54 +0200

    change ruby version in gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,5 @@ Rails.application.routes.draw do
     mount_devise_token_auth_for 'Mentor', at: 'mentor_auth', skip: [:invitations]
     devise_for :mentors, only: [:invitations], controllers: { invitations: 'mentors/invitations' }
 
-  root to: redirect( "http://localhost:3001")
+  root to: redirect( "https://assesmentmakinah.herokuapp.com/")
 end
