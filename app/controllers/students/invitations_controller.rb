6c835e40c6177ff801ee4c6e5deeb04808412268
Pakinam <YOUR@EMAIL.com>
2017-06-01 22:45:28 +0200

    WIP on invitaions

--- app/controllers/students/invitations_controller.rb
+++ app/controllers/students/invitations_controller.rb
@@ -1,41 +1,41 @@
-  class Students::InvitationsController < Devise::InvitationsController
-    include InvitableMethods
-    # before_action :authenticate_user!, only: :create
-    # before_action :resource_from_invitation_token, only: [:edit, :update]
+class Students::InvitationsController < Devise::InvitationsController
+  include InvitableMethods
+  # before_action :authenticate_user!, only: :create
+  # before_action :resource_from_invitation_token, only: [:edit, :update]
 
-    def create
-      Student.invite!(invite_params, current_student)
-      render json: { success: ['Student created.'] }, status: :created
-    end
+  def create
+    Student.invite!(invite_params, current_student)
+    render json: { success: ['Student created.'] }, status: :created
+  end
 
-    def edit
-      redirect_to "#{client_api_url}?invitation_token=#{params[:invitation_token]}"
-    end
+  def edit
+    # redirect_to "#{client_api_url}?invitation_token=#{params[:invitation_token]}"
+  end
 
-    def update
-      student = Student.accept_invitation!(accept_invitation_params)
-      if @student.errors.empty?
-        render json: { success: ['Student updated.'] }, status: :accepted
-      else
-        render json: { errors: student.errors.full_messages },
-               status: :unprocessable_entity
-      end
-    end
+  def update
+    # student = Student.accept_invitation!(accept_invitation_params)
+    # if @student.errors.empty?
+    #   render json: { success: ['Student updated.'] }, status: :accepted
+    # else
+    #   render json: { errors: student.errors.full_messages },
+    #          status: :unprocessable_entity
+    # end
+  end
 
-    def resource_from_invitation_token
-      @student = Student.find_by_invitation_token(params[:invitation_token], true)
-      return if params[:invitation_token] && @student
-      render json: { errors: ['Invalid token.'] }, status: :not_acceptable
-    end
+  def resource_from_invitation_token
+    @student = Student.find_by_invitation_token(params[:invitation_token], true)
+    return if params[:invitation_token] && @student
+    render json: { errors: ['Invalid token.'] }, status: :not_acceptable
+  end
 
-    def authenticate_inviter!
-      # use authenticate_user! in before_action
-    end
+  def authenticate_inviter!
+    # use authenticate_user! in before_action
+  end
 
-    def authenticate_student!
-      return if current_user
-      render json: {
-        errors: ['Authorized users only.']
+  def authenticate_student!
+    return if current_user
+    render json: {
+      errors: ['Authorized users only.']
       }, status: :unauthorized
     end
 
